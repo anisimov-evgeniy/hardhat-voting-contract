e8d418352c2d5352ae081529212b01c63911515a
Анисимов Евгений Владимирович <AnisimovEV@gnivc.ru>
2024-05-19 02:27:27 +0300

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require('dotenv').config();
+require("@nomicfoundation/hardhat-toolbox");
+
+
+const { MNEMONIC } = process.env;
+
+module.exports = {
+  defaultNetwork: "mainnet",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: MNEMONIC}
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.bnbchain.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: MNEMONIC}
+    }
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
